d3fccd2f800578f04514a42be18f059a874a92b5
main <minhnghia1993@gmail.com>
2025-10-30 23:32:24 +0700

    test

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"Hello, this is a test build! - {datetime.datetime.now()}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
