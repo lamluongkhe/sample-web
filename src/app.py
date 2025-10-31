1a60cbb5b3a4cf3fee28c692ca0b2358c24ad63f
main <minhnghia1993@gmail.com>
2025-10-31 18:22:10 +0700

    change code test CI/CD

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"Hello (Khe), this is a test build! - {datetime.datetime.now()}"
+    return f"Hello (Khe New Version), this is a test build! - {datetime.datetime.now()}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
